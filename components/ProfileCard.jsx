b67c823ed747e32fed15036b6837bac8bbf150f8
weng <mager@weng.local>
2023-10-31 04:11:55 +0700

    screen: editing profile screen adding profile card at top

---
+++ components/ProfileCard.jsx
@@ -0,0 +1,97 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+
+const ProfileCard = () => {
+  return (
+    <>
+      <View
+        style={{
+          width: 325,
+          height: 230,
+          alignSelf: "center",
+          marginTop: 20,
+          borderRadius: 10,
+          backgroundColor: "#367A59",
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            width: "100%",
+            padding: 10,
+            justifyContent: "space-between",
+          }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+            <View
+              style={{
+                padding: 2,
+                backgroundColor: "#FFEFCC",
+                borderRadius: 10,
+              }}
+            >
+              <Ionicons name="star" color="#43936C" size={12} />
+            </View>
+            <Text style={{ fontSize: 20, fontWeight: "600", color: "#FFEFCC" }}>
+              700
+            </Text>
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text style={{ fontSize: 12, fontWeight: "400", color: "#FFEFCC" }}>History</Text>
+            <Ionicons name="chevron-forward" color="white" size={12}  />
+          </View>
+        </View>
+        <View
+          style={{
+            backgroundColor: "#090909",
+            flex: 1,
+            borderRadius: 10,
+            padding: 20,
+            gap: 10,
+            alignItems: "center",
+          }}
+        >
+          <View
+            style={{
+              width: 60,
+              height: 60,
+              borderRadius: 40,
+              backgroundColor: "white",
+            }}
+          >
+            <Text>image anjay</Text>
+          </View>
+          <Text style={{ color: "white", fontSize: 20, fontWeight: "600" }}>
+            Wan Hasib Al Aslamy
+          </Text>
+          <Text style={{ color: "white", fontSize: 16, fontWeight: "500" }}>
+            +62 8953 37443659
+          </Text>
+          <View
+            style={{
+              width: "100%",
+              backgroundColor: "#F9F8F8",
+              paddingHorizontal: 10,
+              paddingVertical: 5,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              borderRadius: 20,
+            }}
+          >
+            <Text>Gold Member</Text>
+            <View style={{ flexDirection: "row" }}>
+              <Text>See Benefits</Text>
+              <Ionicons name="chevron-forward" size={16} />
+            </View>
+          </View>
+        </View>
+      </View>
+    </>
+  );
+};
+
+export default ProfileCard;
+
+const styles = StyleSheet.create({});
--- screen/Profile.jsx
+++ screen/Profile.jsx
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import ProfileCard from "../components/ProfileCard";
+import { Ionicons } from "@expo/vector-icons";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <View
+        style={{
+          flexDirection: "row",
+          paddingHorizontal: 20,
+          width: "100%",
+          alignItems: "center",
+          gap: 120,
+        }}
+      >
+        <View
+          style={{ padding: 5, backgroundColor: "#eaeaea", borderRadius: 20 }}
+        >
+          <Ionicons name="chevron-back" size={24} />
+        </View>
+        <Text style={{ fontSize: 24, fontWeight: "bold" }}>Profile</Text>
+      </View>
+      <ProfileCard />
+      <Text>anjay</Text>
+    </SafeAreaView>
+  );
+};
 
-export default Profile
+export default Profile;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
